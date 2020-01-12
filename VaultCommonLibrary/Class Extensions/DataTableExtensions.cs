edf87cc6f56b2ebb55c06b5b687c25a5e7593428
Malekith <jingenito@vcssoftware.com>
2020-01-12 17:25:43 -0500

    added an extension class for datatables to the common library

---
+++ VaultCommonLibrary/Class Extensions/DataTableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Diagnostics;
+
+namespace VaultCommonLibrary
+{
+    public static class DataTableExtensions
+    {
+        [DebuggerStepThrough]
+        public static void AddColumns(this DataTable dt, params string[] columns)
+        {
+            foreach(string column in columns)
+            {
+                dt.Columns.Add(column);
+            }
+        }
+    }
+}
