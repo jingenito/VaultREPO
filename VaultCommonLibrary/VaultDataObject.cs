e4605f343f16ba5e573504499793887bbc45a232
Malekith <jingenito@vcssoftware.com>
2020-01-13 22:34:36 -0500

    added the VaultDataObject class to the common library

---
+++ VaultCommonLibrary/VaultDataObject.cs
@@ -0,0 +1,31 @@
+﻿namespace VaultCommonLibrary
+{
+    public class VaultDataObject
+    {
+        public int RecordNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+        public VaultDataObject() 
+        {
+            this.RecordNumber = 0;
+        }
+
+        public VaultDataObject(string Email,string Password,string UserName)
+        {
+            this.RecordNumber = 0;
+            this.Email = Email;
+            this.Password = Password;
+            this.UserName = UserName;
+        }
+
+        public VaultDataObject(int RecordNumber,string Email,string Password,string UserName)
+        {
+            this.RecordNumber = RecordNumber;
+            this.Email = Email;
+            this.Password = Password;
+            this.UserName = UserName;
+        }
+    }
+}
