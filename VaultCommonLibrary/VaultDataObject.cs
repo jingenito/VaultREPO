3a3775f4213fbfaf56c6e84dcc9a1903a292db3b
Malekith <jingenito@vcssoftware.com>
2020-01-14 20:39:26 -0500

    changed the name to username

--- VaultCommonLibrary/VaultDataObject.cs
+++ VaultCommonLibrary/VaultDataObject.cs
@@ -5,27 +5,19 @@
         public int RecordNumber { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
 
         public VaultDataObject() 
         {
             this.RecordNumber = 0;
         }
 
-        public VaultDataObject(string Email,string Password,string UserName)
-        {
-            this.RecordNumber = 0;
-            this.Email = Email;
-            this.Password = Password;
-            this.UserName = UserName;
-        }
-
-        public VaultDataObject(int RecordNumber,string Email,string Password,string UserName)
+        public VaultDataObject(int RecordNumber, string Email, string Username, string Password)
         {
             this.RecordNumber = RecordNumber;
             this.Email = Email;
             this.Password = Password;
-            this.UserName = UserName;
+            this.Username = Username;
         }
     }
 }
